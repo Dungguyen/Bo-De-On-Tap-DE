7ecad58d631a18b9f509bf6a35c4742520ba1243
Dung Nguyen <dungvippro1711@gmail.com>
2026-09-11 22:47:42 +0530

    Complete Topic 4

--- giaide04.py
+++ giaide04.py
@@ -30,4 +30,97 @@
 29-A
 30-A
 Câu 31:
+    - Chúng ta sẽ chuyển nó về 1 dạng thống nhất là array<object>
+    def normalize_option(option):
+        if option is None:
+            return [], None
+        if isinstance(option, list):
+            return [option], None
+        if isinstance(optin, list) and all(isinstance(x,dict) for x in option):
+            return option, None
+        return None, "INVALID_OPTION_FORMAT"
+
+Câu 32 :
+    - Grain ở đây được xác định như sau, mỗi 1 dòng của bảng là 1 sự kiện đại diện do 1 người dùng hoặc là 1 thiết bị tạo ra cho 1 sản phẩm tại 1 thời điểm cụ thể
+(vd: 1 người dùng đang đại diện cho 1 lựa chọn cho 1 sản phẩm hoặc là 1 thiết bị đang đại diện cho việc lựa chọn sản phẩm )
+    - Chúng ta có thể thiết kể bảng như sau để phù hợp với các event document đã cho : 
+    events
+    -----------------------------
+    even_id          String/ PK
+    event_time       Timestamp
+    event_date       DATE
+    user_id          String
+    device_id        String
+    product_id       String
+    current_url      String
+    Referrer_url     String
+    Collection       String
+
+    - Chill table option
+    event_options 
+    --------------------------------
+    event_id         String
+    option_id        String
+    option_label     String
+    valua_id         String
+    value_label      String
+
+    - Keys :  
+        + events.event_id : PK
+        + event_options.event_id nó là FK của events.event_id
+
+    - Type : 
+    event_id        STRING
+    event_time      TIMESTAMP
+    event_date      DATE
+    user_id         STRING
+    device_id       STRING
+    product_id      STRING
+    current_url     STRING
+    referrer_url    STRING
+
+    option_id       STRING
+    option_label    STRING
+    value_id        STRING
+    value_label     STRING
+
+    - Partition By event_date
+
+    - Trade-off nghĩa là: Khi bạn chọn một phương án, bạn nhận được một số lợi ích nhưng đồng thời phải chấp nhận một số bất lợi.
+Child table
+events
++
+event_options
+
+Ưu điểm:
+
+schema rõ
+SQL analytics dễ
+one-to-many rõ ràng
+option filtering/grouping dễ
+
+Nhược điểm:
+
+tăng số rows
+cần JOIN
+ETL phức tạp hơn
+
+    - Nested array: Nested = dữ liệu con được giữ bên trong record cha. Child table = lấy dữ liệu con đó ra thành bảng riêng rồi liên kết lại bằng key.
+gần source BSON
+ít JOIN
+giữ event + options cùng nhau
+
+Nhược điểm:
+
+phải UNNEST
+query option phức tạp hơn
+engine phải hỗ trợ nested tốt
+
+Một câu trả lời rất ổn trong bài thi là:
+
+Nếu workload thường xuyên phân tích option độc lập, tôi chọn child table. Nếu warehouse hỗ trợ nested tốt và query thường đọc toàn bộ event cùng options, tôi có thể giữ ARRAY<STRUCT>. Quyết định dựa trên query pattern.
+
+Câu 33:   
+
+    
 """
